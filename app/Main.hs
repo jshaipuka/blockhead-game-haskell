b8f2241506ddd5423267a0b8cd34f41a312b6796
yaskovdev <yaskovdev@gmail.com>
2022-11-02 21:31:58 +0200

    Turn the game into a REST service.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Lib
+--import Lib
+import qualified Data.Text.Lazy as L
+import Web.Scotty
+
+--main = startGame
 
 main :: IO ()
-main = startGame
+main = scotty 3000 $ do
+  get "/:word" $ do
+    beam <- param "word"
+    html (L.pack ("response " ++ beam))
+  post "/moves/:move" $ do
+    move <- param "move"
+    html (L.pack ("response " ++ move))
